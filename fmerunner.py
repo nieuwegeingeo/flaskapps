012f41c801ec71e032d9f617dcbb2f71c4e4a191
Richard Duivenvoorde <richard@duif.net>
2017-02-14 10:19:27 +0100

    adding EMAIL

--- fmerunner.py
+++ fmerunner.py
@@ -5,28 +5,7 @@ import subprocess
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='/static')
 
-# from: http://stackoverflow.com/questions/23977475/running-a-shell-command-from-a-flask-app
-
-#@app.route("/")
-def hello():
-    #cmd = ["c:\osgeo4w\oraclespatialwakeup.bat", "-vv"]
-	#"C:\Program Files\FME\fme.exe" C:\temp\OphalenGegevensWebpagina_v1.fmw --COORDS "134151.8183 447006.2135 137810.4403 449482.8191" --VBO_GEB_FUNCTIE "woonfunctie" --OUTPUT_FORMAT "csv" --OUTPUT_DATA ""Adres en Oppervlakte en Gebruiksfunctie""
-    #cmd = ["C:\Program Files\FME\fme.exe", "C:\temp\OphalenGegevensWebpagina_v1.fmw", --COORDS "134151.8183 447006.2135 137810.4403 449482.8191" --VBO_GEB_FUNCTIE "woonfunctie" --OUTPUT_FORMAT "csv" --OUTPUT_DATA ""Adres en Oppervlakte en Gebruiksfunctie""]
-	
-    #cmd = ["C:\Program Files\FME\fme.exe", "D:\zuidt\flask\OphalenGegevensWebpagina_v1.fmw"]
-    #cmd = ["fme"]
-	
-	## LET OP LET OP: ofwel // gebruiken in de windows paden, ofwel \ gebruiken
-	## Anders problemen met bijvoorbeeld /f wat dan wordt vertaald als een formfeed-character https://docs.python.org/2.0/ref/strings.html
-    cmd = ["C:/Program Files/FME/fme.exe", "D:/zuidt/flask/OphalenGegevensWebpagina_v1.fmw", "--COORDS", "134111 447000 137000 449000",  "--VBO_GEB_FUNCTIE", "woonfunctie", "--OUTPUT_FORMAT", "csv", "--OUTPUT_DATA", "Adres en Oppervlakte en Gebruiksfunctie" ]
-    cmd = ["C:\\Program Files\\FME\\fme.exe", "D:\\zuidt\\flask\\OphalenGegevensWebpagina_v1.fmw", "--COORDS", "134111 447000 137000 449000",  "--VBO_GEB_FUNCTIE", "woonfunctie", "--OUTPUT_FORMAT", "csv", "--OUTPUT_DATA", "Adres en Oppervlakte en Gebruiksfunctie" ]	
-    # print cmd
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
-    out,err = p.communicate()
-	
-    return render_template('runapptest.html', out=out)
-    #return out
-    
+# from: http://stackoverflow.com/questions/23977475/running-a-shell-command-from-a-flask-app  
     
 @app.route('/', methods=['GET', 'POST'])
     
@@ -36,28 +15,33 @@ def test():
     vbo_geb_functie = "woonfunctie"
     output_format = "pdf"
     output_data = "Adres en Oppervlakte en Gebruiksfunctie"
+    email = "r.duivenvoorde@nieuwegein.nl"
 
     if request.method == 'POST':
-        #cmd = ["C:\\Program Files\\FME\\fme.exe", "D:\\zuidt\\flask\\OphalenGegevensWebpagina_v1.fmw", "--COORDS", "134111 447000 137000 449000",  "--VBO_GEB_FUNCTIE", "woonfunctie", "--OUTPUT_FORMAT", "csv", "--OUTPUT_DATA", "Adres en Oppervlakte en Gebruiksfunctie" ]
-        
+    
+        # "C:\Program Files\FME\fme.exe" D:\zuidt\flask\OphalenGegevensWebpagina_v1.fmw --EMAIL "r.duivenvoorde@nieuwegein.nl" --COORDS "POLYGON((135213.12 450199.68,135045.12 449984.64,135327.36 449883.84,135508.8 450152.64,135213.12 450199.68))" --VBO_GEB_FUNCTIE "woonfunctie" --OUTPUT_FORMAT "pdf" --OUTPUT_DATA "Adres en Oppervlakte en Gebruiksfunctie"
         
         # get values from form
         coords = request.form['coords']
         vbo_geb_functie = request.form['vbo_geb_functie']
         output_format = request.form['output_format']
         output_data = request.form['output_data']
+        email = request.form['email']
 
       	## LET OP LET OP: ofwel // gebruiken in de windows paden, ofwel \ gebruiken
         ## Anders problemen met bijvoorbeeld /f wat dan wordt vertaald als een formfeed-character https://docs.python.org/2.0/ref/strings.html
-        cmd = ["C:\\Program Files\\FME\\fme.exe", "D:\\zuidt\\flask\\OphalenGegevensWebpagina_v1.fmw", "--COORDS", coords,  "--VBO_GEB_FUNCTIE", vbo_geb_functie, "--OUTPUT_FORMAT", output_format, "--OUTPUT_DATA", output_data ]	
+        
+        cmd = ["C:\\Program Files\\FME\\fme.exe", "D:\\zuidt\\flask\\OphalenGegevensWebpagina_v1.fmw", "--COORDS", coords,  "--VBO_GEB_FUNCTIE", vbo_geb_functie, "--OUTPUT_FORMAT", output_format, "--OUTPUT_DATA", output_data, "--EMAIL", email ]	
         print cmd
         
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
         out,err = p.communicate()
-        return render_template('test_resultaat.html', coords=coords, vbo_geb_functie=vbo_geb_functie, output_format=output_format, output_data=output_data )
+        print out
+        print err
+        return render_template('test_resultaat.html', coords=coords, vbo_geb_functie=vbo_geb_functie, output_format=output_format, output_data=output_data, email=email )
     else:
         # GET: create form
-        return render_template( 'test_invoer.html', coords=coords, vbo_geb_functie=vbo_geb_functie, output_format=output_format, output_data=output_data )
+        return render_template( 'test_invoer.html', coords=coords, vbo_geb_functie=vbo_geb_functie, output_format=output_format, output_data=output_data, email=email )
 	
 if __name__ == "__main__" :
     app.run()
